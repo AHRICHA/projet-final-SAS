867d0ff9ea17a5c4cd217f45685bf4e272c8ed82
Ahricha Mohamed <medhricha5@gmail.com>
2026-09-23 02:18:45 +0100

    project final

---
+++ projet-final-SAS-main/tests/scenarios.js
@@ -0,0 +1,46 @@
+import { apprenants } from '../src/data.js'
+//console.log(apprenants);
+import { normaliserNom } from '../src/progression.js';
+
+// function ajouterApprenanat(id, name, city) {
+//     for (let apprenant of apprenants) {
+//         if (apprenant.id === id) {
+//             console.log("ce apprenant deja existe")
+//             return false;
+//         }
+//     }
+
+//     name = normaliserNom(name)
+//     const nouvelApprenant = {
+//         id: id,
+//         name: name,
+//         city: city,
+//         resultats: []
+//     }
+
+//     apprenants.push(nouvelApprenant)
+
+
+//     return true;
+// }
+// ajouterApprenanat(9, "mohamed", "nador")
+
+// console.log(apprenants)
+
+
+
+
+// function rechercherApprenand(name) {
+//     for (let i = 0; i < apprenants.length; i++) {
+//         if (apprenants[i].nomComplet == normaliserNom(name)) {
+//             return apprenants[i]
+//         }
+//     }
+// }
+
+// console.table(rechercherApprenand("sara dev"));
+
+
+
+
+
